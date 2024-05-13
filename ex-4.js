88bf0e99a0f436748017f5bf31b1da6a160c2caa
CharlieHii <charlie_chaper@hotmail.com>
2024-05-13 12:42:13 +0700

    feat(*.js): complete all exercises

--- ex-4.js
+++ ex-4.js
@@ -12,3 +12,18 @@ const employees = [
 ];
 
 // Start coding here
+employees.push({
+  name: "Alicia",
+  age: 29,
+  hobbies: ["Shopping", "Reading novels"],
+});
+
+employees.push({});
+employees[3].name = "Kody";
+employees[3].age = 19;
+employees[3].hobbies = [];
+
+employees[3].hobbies.unshift("Computer");
+employees[3].hobbies.push("Wakeboard");
+
+console.log(employees);
