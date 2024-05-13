485899cca7f2866e596f2dfda69d2acf118ddb88
Kanokphit <kanokphit55@gmail.com>
2024-05-13 14:21:16 +0700

    feat:Assignment : JavaScript Arrays Assignment

--- ex-7.js
+++ ex-7.js
@@ -42,7 +42,16 @@ let orders = [
 ];
 
 // Start coding here
-let isArray;
-let creditCardTypeOfBlindermann;
-let productQuantityOfJoannet;
-let totalPurchaseOfDary;
+let isArray = Array.isArray(orders);
+let creditCardTypeOfBlindermann = orders[2].creditCardType;
+orders[2].creditCardType = "visa";
+let productQuantityOfJoannet = orders[3].productQuantity;
+let totalPurchaseOfDary = orders[1].productPrice * orders[1].productQuantity;
+delete orders[0];
+console.log(orders);
+
+console.log(isArray);
+console.log(creditCardTypeOfBlindermann);
+console.log(orders[2]);
+console.log(productQuantityOfJoannet);
+console.log(totalPurchaseOfDary);
